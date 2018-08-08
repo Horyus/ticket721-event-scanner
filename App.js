92c43d5a889767d91141aa41219d407ea48a4c27
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2018-08-08 23:21:20 +0200

    Setting Up React Native
    T721COMP

---
+++ App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { View } from 'react-native';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { test } from "./src/redux/test.reducer";
+import { createStackNavigator } from 'react-navigation';
+import {Home} from "./src/views/home";
+import { createNavigationReducer, createReactNavigationReduxMiddleware, reduxifyNavigator } from 'react-navigation-redux-helpers';
+
+const AppNavigator = createStackNavigator(
+    {
+        Home: {
+            screen: Home
+        }
+    },
+    {
+        initialRouteName: 'Home'
+    });
+
+const navReducer = createNavigationReducer(AppNavigator);
+
+const reducer = combineReducers({
+    test: test,
+    nav: navReducer
+});
+
+const middleware = createReactNavigationReduxMiddleware("root", state => state.nav);
+
+const App = reduxifyNavigator(AppNavigator, "root");
+const mapStateToProps = (state) => ({
+    state: state.nav
+});
+
+const AppWithNavigationState = connect(mapStateToProps)(App);
+
+const Store = createStore(reducer, applyMiddleware(middleware));
+
+export default class Root extends React.Component {
+    render() {
+        return (
+            <Provider store={Store}>
+                <AppWithNavigationState/>
+            </Provider>
+        );
+    }
+}
+
